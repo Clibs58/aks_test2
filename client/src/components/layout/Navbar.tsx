680125cf78cce3da4d8531007c5439ffb467cb14
Abhishek Rana <110621025+Clibs58@users.noreply.github.com>
2025-12-10 16:26:50 +0530

    Updated Navbar.tsx

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -30,7 +30,7 @@ export function Navbar() {
       <div className="container mx-auto h-full px-6 flex items-center justify-between">
         <Link href="/" className="cursor-pointer">
   <img 
-    src="client/src/components/layout/aks_logo_website.svg" 
+    src="client/src/components/layout/logo.png" 
     alt="Logo" 
     className="h-10 w-auto"
   />
