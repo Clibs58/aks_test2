3b32fac916dbbbfbf0db838afb07f02a9e6b0d30
Abhishek Rana <110621025+Clibs58@users.noreply.github.com>
2025-12-10 16:37:54 +0530

    Updated Navbar.tsx

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -32,7 +32,7 @@ export function Navbar() {
           <img 
             src="public/aks_logo_website.svg" 
             alt="Logo" 
-            className="h-12 w-auto"
+            className="h-14 w-auto"
           />
         </Link>
 
