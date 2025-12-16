780cd679cfd8c86791b4af1ed3a4777902ccf683
Abhishek Rana <110621025+Clibs58@users.noreply.github.com>
2025-12-16 23:27:03 +0530

    Updated Navbar.tsx

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -8,6 +8,7 @@ const tabs = [
   { label: "Home", id: "home" },
   { label: "About", id: "about" },
   { label: "Portfolio", id: "portfolio" },
+  { label: "Mentors", id: 'mentors'}
   { label: "Contact", id: "contact" },
 ];
 
